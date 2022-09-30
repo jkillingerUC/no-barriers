303006be054973b9bd26f5a06d658b78d4907fa2
Daniel Goebel <dgoebel@uchicago.edu>
2022-09-30 15:17:30 -0500

    test of the hp with a small update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 const Page = () => {
   return (
     <main>
-      <h1>Index Page</h1>
+      <h1>Index Page, perhaps</h1>
       <p>
         This is an example template for helping you to learn how to build a site
         powered by Gatsby. No way this changes live. Nevermind. Still?Thank god
